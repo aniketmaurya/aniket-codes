aeca67c54820c4809f3128be1fff499e1c733466
Aniket Maurya <aniketmaurya@outlook.com>
2023-12-04 16:11:29 +0000

    benchmark

--- llms/benchmark/vllm/benchmark.py
+++ llms/benchmark/vllm/benchmark.py
@@ -4,13 +4,13 @@ import time
 import os
 from prompts import questions
 
-tensor_parallel_size = os.environ.get("DEVICES", 1)
+tensor_parallel_size = int(os.environ.get("DEVICES", "1"))
 print("tensor_parallel_size: ", tensor_parallel_size)
 
 sampling_params = SamplingParams(max_tokens=200)
 
 llm = LLM("meta-llama/Llama-2-7b-hf", tensor_parallel_size=tensor_parallel_size)
-llm("This is me warming up the model", sampling_params=sampling_params)
+print(llm.generate("This is me warming up the model", sampling_params=sampling_params))
 
 results = []
 for q in questions:
@@ -20,4 +20,4 @@ for q in questions:
     results.append({"time": t1-t0, "tokens_generated": len(output.outputs[0].token_ids)})
 
 df = pd.DataFrame(results)
-df.to_csv(f"benchmark-{tensor_parallel_size}GPUs.csv", index=False)
+df.to_csv(f"vllm-benchmark-{tensor_parallel_size}GPUs.csv", index=False)
